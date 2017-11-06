eddd25cd78680d422fa7fc8860f5e5e7c0f8825e
wim <van.laer.wim@gmail.com>
2017-11-06 09:16:28 +0100

    EditableTextBlock First

---
+++ DatabaseManager/Views/Converters/ObjectConverters.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DatabaseManager.Views.Converters
+{
+    public class ObjectToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value?.ToString();
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class ObjectToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return System.Convert.ToDouble(value);
+            }
+            catch (FormatException)
+            {
+            }
+            catch (InvalidCastException)
+            {
+            }
+            catch (OverflowException)
+            {
+            }
+            return default(double);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) 
+            => value;
+    }
+
+    public class ObjectToCharConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var s = value?.ToString();
+            return s?.Length == 1
+                ? s[0]
+                : default(char);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) 
+            => value;
+    }
+}
