1d5de6fbe4697b166cd6f57ff32ab5584590ba90
wim <van.laer.wim@gmail.com>
2017-11-07 10:11:04 +0100

    ObjectEditor first

---
+++ DatabaseManager/Views/Controls/ObjectEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DatabaseManager.Views.Controls
+{
+    public class ObjectEditor : Control
+    {
+        #region DEPENDENCY PROPERTIES
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+            nameof(Value),
+            typeof(object),
+            typeof(ObjectEditor),
+            new PropertyMetadata(default(object)));
+
+        #endregion DEPENDENCY PROPERTIES
+
+        #region PROPERTIES
+
+        public object Value
+        {
+            get => GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        #endregion PROPERTIES
+
+        #region CONSTRUCTORS
+
+        static ObjectEditor()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ObjectEditor),
+                new FrameworkPropertyMetadata(typeof(ObjectEditor)));
+        }
+
+        #endregion CONSTRUCTORS
+
+        #region METHODS
+
+        #endregion METHODS
+    }
+}
