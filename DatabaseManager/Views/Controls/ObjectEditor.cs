f4a57df7963ed9d8d7a66d2ba296ea44360b78a8
wim <van.laer.wim@gmail.com>
2017-11-09 19:37:02 +0100

    objecteditor added properties

--- DatabaseManager/Views/Controls/ObjectEditor.cs
+++ DatabaseManager/Views/Controls/ObjectEditor.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+using ClassLibrary.Portable.Extensions;
 
 namespace DatabaseManager.Views.Controls
 {
@@ -8,10 +11,23 @@ namespace DatabaseManager.Views.Controls
         #region DEPENDENCY PROPERTIES
 
         public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
-            nameof(Value),
-            typeof(object),
-            typeof(ObjectEditor),
-            new PropertyMetadata(default(object)));
+            nameof(Value), typeof(object), typeof(ObjectEditor), new PropertyMetadata(default(object), OnValueChanged));
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+            nameof(Title), typeof(Property), typeof(ObjectEditor), new PropertyMetadata(default(Property)));
+
+        public static readonly DependencyProperty SubtitleProperty = DependencyProperty.Register(
+            nameof(Subtitle), typeof(Property), typeof(ObjectEditor), new PropertyMetadata(default(Property)));
+
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+            nameof(Description), typeof(Property), typeof(ObjectEditor), new PropertyMetadata(default(Property)));
+
+        public static readonly DependencyProperty PropertiesProperty = DependencyProperty.Register(
+            nameof(Properties), typeof(IEnumerable<Property>), typeof(ObjectEditor),
+            new PropertyMetadata(default(IEnumerable<Property>)));
+
+        public static readonly DependencyProperty TabsProperty = DependencyProperty.Register(
+            "Tabs", typeof(IEnumerable), typeof(ObjectEditor), new PropertyMetadata(default(IEnumerable)));
 
         #endregion DEPENDENCY PROPERTIES
 
@@ -22,6 +38,32 @@ namespace DatabaseManager.Views.Controls
             get => GetValue(ValueProperty);
             set => SetValue(ValueProperty, value);
         }
+
+        public Property Title
+        {
+            get => (Property) GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public Property Subtitle
+        {
+            get => (Property) GetValue(SubtitleProperty);
+            set => SetValue(SubtitleProperty, value);
+        }
+
+        public Property Description
+        {
+            get => (Property) GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public IEnumerable<Property> Properties
+        {
+            get => (IEnumerable<Property>) GetValue(PropertiesProperty);
+            set => SetValue(PropertiesProperty, value);
+        }
+        
+        public IEnumerable<Property> NativeProperties { get; set; }
 
         #endregion PROPERTIES
 
@@ -36,6 +78,39 @@ namespace DatabaseManager.Views.Controls
         #endregion CONSTRUCTORS
 
         #region METHODS
+
+        private static void OnValueChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var model = obj.GetValue(ValueProperty);
+            var properties = Property.ConvertToProperties(model.GetType().GetProperties(), model).ToCPList();
+
+            var headerProperties = GetAndSetHeaderProperties(obj, properties);
+            properties -= headerProperties;
+
+            obj.SetValue(PropertiesProperty, properties);
+        }
+
+        private static IEnumerable<Property> GetAndSetHeaderProperties(DependencyObject obj,
+            IEnumerable<Property> properties)
+        {
+            var ret = new ClassLibrary.Portable.Collections.List<Property>();
+
+            foreach (var p in properties)
+            {
+                if (p.IsTitle)
+                    obj.SetValue(TitleProperty, p);
+                else if (p.IsSubTitle)
+                    obj.SetValue(SubtitleProperty, p);
+                else if (p.IsDescription)
+                    obj.SetValue(DescriptionProperty, p);
+                else
+                    continue;
+
+                ret += p;
+            }
+
+            return ret;
+        }
 
         #endregion METHODS
     }
