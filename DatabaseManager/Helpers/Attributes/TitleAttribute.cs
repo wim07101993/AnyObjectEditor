c2e8b72346fcfa4f41b7e6b210109257f551f8ce
wim <van.laer.wim@gmail.com>
2017-12-21 16:39:29 +0100

    title, subtitle, description, picture attributes created

---
+++ DatabaseManager/Helpers/Attributes/TitleAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DatabaseManager.Helpers.Attributes
+{
+    public class TitleAttribute : Attribute
+    {
+        public static readonly TitleAttribute Yes = new TitleAttribute(true);
+        public static readonly TitleAttribute No = new TitleAttribute(false);
+        public static readonly TitleAttribute Default = No;
+
+        public bool Title { get; } = true;
+
+        public TitleAttribute()
+        {
+        }
+
+        public TitleAttribute(bool title)
+        {
+            Title = title;
+        }
+
+        public override bool Equals(object obj)
+            => obj == this || obj is TitleAttribute other && other.Title == Title;
+
+        public override int GetHashCode()
+            => Title.GetHashCode();
+    }
+}
