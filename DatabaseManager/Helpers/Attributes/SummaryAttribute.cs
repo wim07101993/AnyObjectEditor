19ef64ae6b2828fcfef4b0cabaf4715ace3e0d28
wim <van.laer.wim@gmail.com>
2017-12-21 16:41:55 +0100

    summary attribute created

---
+++ DatabaseManager/Helpers/Attributes/SummaryAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DatabaseManager.Helpers.Attributes
+{
+    public class SummaryAttribute : Attribute
+    {
+        public static readonly SummaryAttribute Yes = new SummaryAttribute(true);
+        public static readonly SummaryAttribute No = new SummaryAttribute(false);
+        public static readonly SummaryAttribute Default = No;
+
+        public bool Summary { get; } = true;
+
+        public SummaryAttribute()
+        {
+        }
+
+        public SummaryAttribute(bool summary)
+        {
+            Summary = summary;
+        }
+
+        public override bool Equals(object obj)
+            => obj == this || obj is SummaryAttribute other && other.Summary == Summary;
+
+        public override int GetHashCode()
+            => Summary.GetHashCode();
+    }
+}
+}
