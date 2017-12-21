c0c7eba16de29ed293b4031716db5ae3b2da6448
wim <van.laer.wim@gmail.com>
2017-12-21 19:42:42 +0100

    PropertyViewModel created

---
+++ DatabaseManager/ViewModel/PropertyViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DatabaseManager.Helpers.Extensions;
+using DatabaseManager.ViewModelInterfaces;
+using Prism.Mvvm;
+
+namespace DatabaseManager.ViewModel
+{
+    public class PropertyViewModel : BindableBase, IPropertyViewModel
+    {
+        #region FIELDS
+
+        private object _value;
+
+        #endregion FIELDS
+
+        #region PROPERTIES
+
+        public string Name { get; }
+
+        public object Value
+        {
+            get => _value;
+            set => SetProperty(ref _value, value);
+        }
+
+        public Type Type { get; }
+
+        public bool IsBrowsable { get; } = true;
+        public bool IsTitle { get; }
+        public bool IsSubTitle { get; }
+        public bool IsDescription { get; }
+        public bool IsReadOnly { get; }
+
+        #endregion PROPERTIES
+
+        #region CONSTRUCTORS
+
+        public PropertyViewModel()
+        {
+        }
+
+        public PropertyViewModel(PropertyInfo propertyInfo, object parent) : this()
+        {
+            Name = propertyInfo.GetDisplayName();
+            Value = propertyInfo.GetValue(parent);
+            Type = propertyInfo.PropertyType;
+
+            IsBrowsable = propertyInfo.IsBrowsable();
+            IsTitle = propertyInfo.IsTitle();
+            IsSubTitle = propertyInfo.IsSubtitle();
+            IsDescription = propertyInfo.IsDescription();
+
+            IsReadOnly = propertyInfo.CanWrite;
+        }
+
+        #endregion CONSTRUCTORS
+
+        #region METHODS
+
+        public static IEnumerable<IPropertyViewModel> ConvertToProperties(IEnumerable<PropertyInfo> propertyInfos,
+            object parent)
+            => propertyInfos.Select(x => new PropertyViewModel(x, parent));
+
+        public static IEnumerable<IPropertyViewModel> GetPropertiesFromObject(object parent)
+            => ConvertToProperties(parent.GetType().GetProperties(), parent);
+
+        protected override bool SetProperty<T>(ref T storage, T value, string propertyName = null)
+            => base.SetProperty(ref storage, value, Name);
+
+        #endregion METHODS
+    }
+}
