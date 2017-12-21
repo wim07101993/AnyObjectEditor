d3dbb61cdd82caaa06f1cdc533be076cfeae30fd
wim <van.laer.wim@gmail.com>
2017-12-21 15:23:34 +0100

    Person Model created

---
+++ DatabaseManager/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Media.Imaging;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DatabaseManager.Models
+{
+    public class Person
+    {
+        [BsonId]
+        public ObjectId ID { get; set; }
+
+        [BsonElement("firstName")]
+        public string FirstName { get; set; }
+
+        [BsonElement("lastName")]
+        public string LastName { get; set; }
+
+        [BsonElement("age")]
+        public int Age { get; set; }
+
+        [BsonElement("picture")]
+        public BitmapSource Picture { get; set; }
+
+        [BsonElement("summary")]
+        public string Summary { get; set; }
+    }
+}
