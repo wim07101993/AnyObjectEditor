e3b0cf45f234b8595cbae225d843df589848ecde
wim <van.laer.wim@gmail.com>
2018-01-06 18:37:50 +0100

    Changed header of add item

---
+++ DatabaseManager/Helpers/Converters/InvertedNumberEqualityToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DatabaseManager.Helpers.Converters
+{
+    public class InvertedNumberEqualityToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var p = System.Convert.ToDouble(parameter);
+                var v = System.Convert.ToDouble(value);
+                return Math.Abs(p - v) < 0.000001
+                    ? Visibility.Collapsed
+                    : Visibility.Visible;
+            }
+            catch (Exception)
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
